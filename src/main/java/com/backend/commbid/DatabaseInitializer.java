7fea6afd39e4fd45116d64f39010481e8ff7e155
Wiktor Heblewski <240681@edu.p.lodz.pl>
2024-12-09 22:59:26 +0100

    fix init database data

--- src/main/java/com/backend/commbid/DatabaseInitializer.java
+++ src/main/java/com/backend/commbid/DatabaseInitializer.java
@@ -4,6 +4,8 @@ import com.backend.commbid.models.User;
 import com.backend.commbid.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
@@ -19,7 +21,9 @@ public class DatabaseInitializer {
                 // Create and save initial users
                 User user1 = new User("JohnDoe", "johndoe@example.com", "hashedPassword1", null, false, false, null, null, new Timestamp(System.currentTimeMillis()));
                 User user2 = new User("JaneSmith", "janesmith@example.com", "hashedPassword2", null, true, true, null, null, new Timestamp(System.currentTimeMillis()));
-
+                PasswordEncoder encoder = new BCryptPasswordEncoder();
+                user1.setPassword(encoder.encode(user1.getPassword()));
+                user2.setPassword(encoder.encode(user2.getPassword()));
                 userRepository.save(user1);
                 userRepository.save(user2);
 
